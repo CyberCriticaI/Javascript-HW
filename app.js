9245ffbf5f4472cdcc2e5a737b467cd490567c87
Christian Izaguirre <173810334+CyberCriticaI@users.noreply.github.com>
2024-07-23 23:04:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const a =1
+const b =2
+const c =3
+const d =4
+const e =5
+const f =6
+const g =7
+const h =8
+const i =9
+const j =10
+// 10 Variables
+
+// Calculations to create new Variables
+Var1 = (a + b);
+console.log(Var1);
+Var2 = (d - c);
+console.log(Var2)
+Var3 = (e * f);
+console.log(Var3)
+Var4 = (j / b);
+console.log(Var4)
+Var5 = (h + c && i * b);
+console.log(Var5)
---
+++ node.js
@@ -0,0 +1,36 @@
+// 10 Variables
+const a =1
+const b =2
+const c =3
+const d =4
+const e =5
+const f =6
+const g =7
+const h =8
+const i =9
+const j =10
+
+// Calculations to create new Variables
+Var1 = (a + b);
+console.log(Var1);
+Var2 = (d - c);
+console.log(Var2)
+Var3 = (e * f);
+console.log(Var3)
+Var4 = (j / b);
+console.log(Var4)
+Var5 = (h + c && i * b);
+console.log(Var5)
+
+// Create 3 if/else statements
+if (a === 1) {
+    console.log("Good Job, you wrote the same number!")
+}else {
+    console.log("AYO THIS AINT NUMBER 1")
+}
+
+if (Var1 * Var4 === Var5) {
+  console.log("I didn't even know you could do that")
+}else{
+  console.log("I am assuming its going to do this command")
+}
